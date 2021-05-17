2067e28ef5cf33606af4f2cb2080b2155ed4019a
Terence Mathieu <71327314+TYMathieu@users.noreply.github.com>
2021-05-17 14:10:57 +0200

    update

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,23 @@ class SessionController extends AbstractController
      */
     public function index(): Response
     {
+        $sessions = $this->getDoctrine()
+            ->getRepository(Session::class)
+            ->findBy([], ['id' => 'ASC']);
+
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}", name="session_show")
+     */
+    public function show(Session $session): Response
+    {
+        return $this->render('session/show.html.twig', [
+            'session' => $session
         ]);
     }
 }
