ef5ece94dfc3a14c34ce77f284146d7b5f004a7b
Terence Mathieu <71327314+TYMathieu@users.noreply.github.com>
2021-05-17 15:01:15 +0200

    update

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Form\SessionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,6 +22,35 @@ class SessionController extends AbstractController
 
         return $this->render('session/index.html.twig', [
             'sessions' => $sessions
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="session_add")
+     * @Route("/edit/{id}", name="session_edit")
+     */
+    public function new(Request $request, Session $session = null): Response
+    {
+        if (!$session) {
+            $session = new Session();
+        }
+
+        $form = $this->createForm(SessionType::class, $session);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $session = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($session);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('session');
+        }
+
+        return $this->render('session/new.html.twig', [
+            'formAddSession' => $form->createView(),
         ]);
     }
 
