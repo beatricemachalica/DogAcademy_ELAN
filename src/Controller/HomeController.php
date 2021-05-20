d0e7f9239be893a0f1f1d9533a8abd7fcfae54c2
beatricemachalica <beatka.macha68@yahoo.fr>
2021-05-20 13:49:15 +0200

    update sign in sign up logout

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
