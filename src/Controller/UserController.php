70778ed7eff6a7a547480ab9da83cb9bae165554
beatricemachalica <beatka.macha68@yahoo.fr>
2021-05-25 11:44:51 +0200

    update account

---
+++ src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/user")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/compte", name="user_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $this->getUser()->getUsername(),
+        ]);
+    }
+}
