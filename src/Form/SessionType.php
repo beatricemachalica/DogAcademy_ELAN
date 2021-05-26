40000ae2a05961b64c80c7c41a3d4e702bde44bb
Terence Mathieu <71327314+TYMathieu@users.noreply.github.com>
2021-05-26 14:23:21 +0200

    update

--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -2,18 +2,19 @@
 
 namespace App\Form;
 
-use App\Entity\Session;
 use App\Entity\Chien;
+use App\Entity\Session;
 use App\Entity\Formation;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Form\AteliersType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SessionType extends AbstractType
 {
@@ -39,7 +40,6 @@ class SessionType extends AbstractType
         'required' => false,
         'allow_add' => true,
         'allow_delete' => true,
-        'attr' => ['class' => 'uk-input'],
       ])
       ->add('dateDebut', DateType::class, [
         'label' => 'Date de début',
@@ -48,6 +48,16 @@ class SessionType extends AbstractType
       ->add('dateFin', DateType::class, [
         'label' => 'Date de fin',
         'widget' => 'single_text',
+      ])
+      ->add('programmers', CollectionType::class, [
+        'label' => false,
+        'entry_type' => AteliersType::class,
+        'entry_options' => [
+          'label' => "Module et durée : "
+        ],
+        'allow_add' => true,
+        'allow_delete' => true,
+        'by_reference' => false,
       ]);
     // not needed anymore
     // ->add('envoyer', SubmitType::class, [
